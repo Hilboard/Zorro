c9f1f803ac9d1cf1d92c997e684d9a84e17249ba
artem <akhilya@yandex.ru>
2025-07-25 22:26:21 +0300

    Chore: Throw query removed TEntry collection type

--- Query/Essentials/ThrowQuery.cs
+++ Query/Essentials/ThrowQuery.cs
@@ -4,7 +4,7 @@ namespace Zorro.Query.Essentials;
 
 public static class ThrowQuery
 {
-    public static QueryContext Throw<TEntry>(this QueryContext context, QueryException exception)
+    public static QueryContext Throw(this QueryContext context, QueryException exception)
     {
         throw exception;
     }
