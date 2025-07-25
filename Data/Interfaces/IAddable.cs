89ff629ca4b912d6e1c648feb59483e0f504421c
artem <akhilya@yandex.ru>
2025-07-25 22:32:08 +0300

    Chore: IAddable and IUpdateable change collection type requirement

--- Data/Interfaces/IAddable.cs
+++ Data/Interfaces/IAddable.cs
@@ -1,6 +1,6 @@
 namespace Zorro.Data.Interfaces;
 
-public interface IAddable<TForm> where TForm : struct
+public interface IAddable<TForm> where TForm : notnull
 {
     public bool AddFill(TForm form);
 }
--- Data/Interfaces/IUpdateable.cs
+++ Data/Interfaces/IUpdateable.cs
@@ -1,6 +1,6 @@
 namespace Zorro.Data.Interfaces;
 
-public interface IUpdateable<TForm> where TForm : struct
+public interface IUpdateable<TForm> where TForm : notnull
 {
     public bool UpdateFill(TForm form);
 }
